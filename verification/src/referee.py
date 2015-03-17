de1ed2c4b6494750b0b77a4218d7467158d7ad83
Valentin Bryukhanov <bvv.mag@gmail.com>
2015-03-17 08:23:52 +0300

    add representation

--- verification/src/referee.py
+++ verification/src/referee.py
@@ -1,4 +1,5 @@
 from checkio_referee import RefereeBase
+from checkio_referee import representations
 
 import settings
 import settings_env
@@ -10,11 +11,22 @@ def cover(func, data):
 """
 
 
+def py_tuple_od_tuples(test, function_name):
+    data = test["input"]
+    return "{}({})".format(function_name,
+                           tuple(tuple(row) for row in data[0]), tuple(data[1]), tuple(data[2]))
+
+
 class Referee(RefereeBase):
     TESTS = TESTS
     EXECUTABLE_PATH = settings.EXECUTABLE_PATH
     CURRENT_ENV = settings_env.CURRENT_ENV
     FUNCTION_NAME = "can_pass"
+    CALLED_REPRESENTATIONS = {
+        "python_3": py_tuple_od_tuples,
+        "python_2": py_tuple_od_tuples,
+        "javascript": representations.unwrap_arg_representation
+    }
     ENV_COVERCODE = {
         "python_2": cover,
         "python_3": cover,
